d26de14327abd4b7b7c3ac58f9b26eb8522bc789
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-04-03 12:32:39 +0200

    Updating repository

---
+++ src/pl/akademiakodu/zad1/Main.java
@@ -0,0 +1,24 @@
+package pl.akademiakodu.zad1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Zadanie 1-a");
+        System.out.println("Łukasz");
+        System.out.println("Kołacz");
+
+        System.out.println("Zadanie 1-b");
+        String name = "Łukasz";
+        String surname = "Kołacz";
+        System.out.println(name);
+        System.out.println(surname);
+
+        System.out.println("Zadanie 1-c");
+        System.out.println("Wiek: " + 27);
+
+        System.out.println("Zadanie 1-d");
+        int age = 27;
+        System.out.println("Wiek: " + age);
+    }
+}
